5a26d79c8864c33ea669a30a541db731ef34c0fc
Clara Derot <90332021+claradrt@users.noreply.github.com>
2021-12-03 15:41:02 +0100

    Fixed React warning

--- src/Results.js
+++ src/Results.js
@@ -3,7 +3,6 @@ import Meaning from "./Meaning.js";
 
 export default function Results(props) {
   console.log(props.results);
-  let definition = props.results.meanings[0].definitions[0].definition;
   return (
     <div>
       <h2 className="text-capitalize">{props.results.word}</h2>
