a45d78643e19dc725f057d9bee28911b49283f3f
Clara Derot <90332021+claradrt@users.noreply.github.com>
2021-12-06 11:04:40 +0100

    Styled synonyms list

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "./Synonyms.css";
 
 export default function Synonyms(props) {
   console.log("Synonyms:", props.synonyms);
   if (props.synonyms.length > 0) {
     return (
-      <ul className="Synonyms">
+      <ul className="SynonymList list-group list-group-horizontal-sm">
         {props.synonyms.map((synonym, index) => {
-          return <li key={index}>{synonym} </li>;
+          return (
+            <li key={index} className="synonyms list-group-item">
+              {synonym}{" "}
+            </li>
+          );
         })}
       </ul>
     );
