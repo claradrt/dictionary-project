6c0b940908476e863583ff4504761315f8bb05d2
Clara Derot <90332021+claradrt@users.noreply.github.com>
2021-12-09 09:54:03 +0100

    Add audio feature

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,10 +5,16 @@ import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 import { faPlayCircle as farPlayCircle } from "@fortawesome/free-regular-svg-icons";
 
 export default function Phonetic(props) {
+  function handleAudioClick(event) {
+    event.preventDefault();
+    const audio = new Audio(props.phonetic.audio);
+    audio.play();
+  }
+
   return (
     <span className="Phonetics">
       {props.phonetic.audio && (
-        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        <a href="#" onClick={handleAudioClick}>
           <span className="icon" id="full-play-icon">
             <FontAwesomeIcon icon={faPlayCircle} />
           </span>
